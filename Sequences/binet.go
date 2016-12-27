deee708aaeb1dcbf4129374a1dda526458184471
Daniel <dfsantosb@correo.udistrital.edu.co>
2016-12-27 10:50:52 -0500

    Create folder to storage the sequences' algorithms and add Binet's formula for fibonacci numbers

---
+++ Sequences/binet.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+* Compute the nth fibonacci number using the binet's formula
+ */
+func nth_fibo(n int) (nth float64) {
+	fi := (1 + math.Sqrt(5)) / 2
+	ga := (1 - math.Sqrt(5)) / 2
+	nth = (math.Pow(fi, float64(n)) - math.Pow(ga, float64(n))) / (math.Sqrt(5))
+	return
+}
+
+func main() {
+	n := 100
+	fmt.Println(" nth fibo =  ", nth_fibo(n))
+}
