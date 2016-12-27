f6332bc48b163eb100b56ae62f7f0344d2d22042
Daniel <dfsantosb@correo.udistrital.edu.co>
2016-12-27 14:10:37 -0500

    Create folder for algorithm over arrays and create algorithm for permutations in java and python

---
+++ Arrays/Permutation.py
@@ -0,0 +1,11 @@
+def permutation(array, start = 0):
+    if (start == len(array)):
+        print(array)
+        return
+    for i in range(start, len(array)):
+        array[start], array[i] = array[i], array[start]
+        permutation(array, start + 1)
+        array[start], array[i] = array[i], array[start]
+
+permutation(['d','a','n'])
+    
