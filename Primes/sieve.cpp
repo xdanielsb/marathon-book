d68ce31d7352b1eb99fd5eaa3aafd1b164c3b323
Daniel <dfsantosb@correo.udistrital.edu.co>
2016-12-28 21:16:04 -0500

    Add the sieve for seeking primes

---
+++ Primes/sieve.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+#define tam 1000
+
+using namespace std;
+typedef long long ll;
+typedef vector< bool > vbool;
+
+
+void show (vbool primes){
+    int cap = primes.size();
+    cout << cap << endl;
+    for(int i = 0; i< cap; i++){
+        cout << i << " : " << primes[i] << endl;
+    }
+
+}
+vbool sieve(ll n){
+   
+    vbool sieve (tam);
+
+    for (int i = 0; i < tam; i++)
+        sieve[i] = true;
+
+    //default values
+    sieve [0] = sieve[1] = false;
+
+    ll root = sqrt(n);
+
+    for (int i = 2; i < root; i++){ //find primes
+        if(sieve[i]){
+            //removes all the multiples of the current prime
+            for (int k = i*i; k<= n; k+=i){ 
+                sieve[k] = false;
+            }
+        }
+    }
+    cout << "tam " << sieve.size() << endl;
+    return sieve;
+
+}
+
+
+int main(){
+    //Initialize the array
+    vbool primes = sieve(1000);
+    show(primes);;
+    primes.clear();
+
+    return 0;
+}
