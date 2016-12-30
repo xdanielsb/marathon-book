0a7f97481fe63cc8aab7804d528f7a05b1a3877a
Daniel <dfsantosb@correo.udistrital.edu.co>
2016-12-30 14:24:58 -0500

    Add usefull script for measure the time execution.

---
+++ snippets/time.cpp
@@ -0,0 +1,15 @@
+#include <chrono>
+#include <iostream>
+using namespace std;
+int main(){
+    
+    auto start = chrono::high_resolution_clock::now();
+    for(long long i = 0; i < 10000000; i++)
+        continue;
+        
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> diff = end-start;
+    cout << diff.count() << endl ;
+
+    return 0;
+}
