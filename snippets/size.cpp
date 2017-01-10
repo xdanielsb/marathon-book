dc97023244e397e0456107d9161fe9960b7fce89
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-01-09 20:06:00 -0500

    Add awesome for size in c.

---
+++ snippets/size.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+#define sz(a) ((int)(a).size())
+
+using namespace std;
+
+int main(){
+
+    string t = "Hello, what's up";
+    vector<int> c (10);
+
+    cout << sz(t) << endl;
+    cout << sz(c) << endl;
+
+}
