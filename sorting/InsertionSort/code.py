1868d2b6641501dcbf0bde163b8f529de34f6663
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-01-12 13:54:23 -0500

    Create module for storing sortings algorithms and implement the code for insertion sort

---
+++ sorting/InsertionSort/code.py
@@ -0,0 +1,27 @@
+
+def show(array):
+    for element in array:
+        print(element, end = " ")
+    print("")
+
+
+def sort(array, length_array):
+    for index in range(1,length_array):
+        key = array[index]
+        index_aux = index -1
+        while index_aux >=0 and array[index_aux]>key:
+            array[index_aux+1] = array[index_aux]
+            index_aux = index_aux -1
+        array[index_aux+1]=key
+        
+
+if __name__ == "__main__":
+    array = [50, 885, 1 , -8 , 54 , 2, 54, 0]
+    print("Original array")
+    show(array)
+    print("Sorted array")
+    sort(array, len(array))
+    print(array)
+    
+
+
