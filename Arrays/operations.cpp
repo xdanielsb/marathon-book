d31a80b30dd59a2b08b10dcd83489c8952dfe456
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-03-04 17:25:17 -0500

    Add simple algorithm over c++ to get the permutations.

---
+++ Arrays/operations.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+typedef vector <int > vi;
+
+inline void show(vi &data, int &size){
+    for (int i=0; i<size; i++)
+            printf("%d \t", data[i]);
+        printf("\n");
+}
+inline void permutation(vi data, int size){
+    sort(data.begin(), data.end());
+    do {
+        show(data, size);
+    }while(next_permutation(data.begin(), data.end()));
+    show(data, size);
+}
+
+int main(){
+    int size = 3 ;
+    int data[] = {1,4,-1};
+    vi vals(begin(data), end(data));
+    permutation(vals, size);
+    return 0;
+}
