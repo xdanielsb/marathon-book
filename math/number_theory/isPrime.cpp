183e8b9ce94b81d2dcd03b0f4af41a7d9f40bec0
David Castelblanco Benavides <cbdavides@gmail.com>
2017-03-11 20:32:28 -0500

    Se modifica el algoritmo que verifica si un número es primo.

--- math/number_theory/isPrime.cpp
+++ math/number_theory/isPrime.cpp
@@ -4,19 +4,16 @@
 using namespace std;
 typedef long long ll;
 
-
 bool is_prime(ll n){
-    if (n<=1) return false;
-    if (n==2) return true;
-    if (n%2==0) return false;
-    ll root = sqrt(n);
-    for (int i=3; i<= root; i+=2){
-        if(n%i == 0 )
+    if (n < 2) return false;
+    if (n < 4) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    if (n < 25) return true;
+    for(int i = 5; i*i <= n; i += 6){
+        if(n % i == 0 || n % (i + 2) == 0)
             return false;
     }
     return true;
-
-
 }
 int main(){
     cout << is_prime(23234) << endl;
