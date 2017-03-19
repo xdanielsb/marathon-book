a6385f6269c1b4a6fcca7afe814d196e08d436f7
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-03-19 05:50:34 -0500

    Add simple algorithm implementing Bubble sort.

---
+++ Sorting/BubbleSort/buble.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define forn(i,j,k) for (int i=j; i<k; i++)
+using namespace std;
+
+typedef long long ll;
+
+inline void sort(ll *arr, int size){
+    forn(i,0,size-1)
+        forn(j,0, size-i-1)
+            if (arr[j] > arr[j+1])
+                swap(arr[j], arr[j+1]);
+}
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int size =8;
+    ll *data  = new ll[size];
+    
+    forn(i, 0, size)
+        scanf("%lld", &data[i]);
+
+    sort(data, size);
+    
+    forn(i, 0, size)
+        printf("%lld ",data[i]);
+    
+    
+    return 0;
+
+}
