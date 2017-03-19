7ada8fda9086823645199af3ac0d36d744605209
Daniel <dfsantosb@correo.udistrital.edu.co>
2017-03-19 05:27:46 -0500

    Add linear method for finding the maximun subarray  sum.

--- Arrays/maximun_subarray_problem/methods.cpp
+++ Arrays/maximun_subarray_problem/methods.cpp
@@ -1,26 +1,24 @@
 #include <bits/stdc++.h>
 #define forn(i,j,k) for(int i=j; i<k; i++)
 using namespace std;
-/*
- * First method 
- * Complexity O(n^3)
- */
 
 typedef long long ll;
 
+
+/*
+ * First method 
+ * Complexity O(n)
+ */
+
 inline ll get_max_sum(ll * data, int size){
-    ll maxn =0, aux;
+    ll maxn =0, aux = 0;
     forn(i, 0, size){
-        forn(j, i, size){
-            aux =0;
-            forn(k, i, j){
-                aux += (ll) data[k];
-            }
-            maxn = max(maxn, aux);
-        }
+        aux = max(data[i], data[i]+aux);
+        maxn = max(maxn, aux);
     }
     return maxn;
 }
+
 
 int main(){
 
