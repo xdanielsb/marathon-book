6f654b192f42af796fca941584eab0f110dcf349
David Castelblanco Benavides <cbdavides@gmail.com>
2017-03-20 21:20:56 -0500

    The implementation of Union Find Disjoint Sets has been changed to a .cpp file.

---
+++ Structures/disjoint_sets.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void initialize(int *arr, int *size, int n) {
+    for(int i=0; i<n; i++){
+        arr[i] = i;
+        size[i] = 1;
+    }
+}
+
+int find(int *arr, int i) {
+    if(arr[i] == i) {
+        return i;
+    } else {
+        arr[i] = find(arr, i);
+        return arr[i];
+    }
+}
+
+void unite(int *arr, int *size, int a, int b) {
+    int root_a = find(arr, a);
+    int root_b = find(arr, b);
+
+    if(root_a == root_b)
+        return;
+
+    if(size[root_a] < size[root_b]) {
+        arr[root_a] = arr[root_b];
+        size[root_b] += arr[root_a];
+    } else {
+        arr[root_b] = arr[root_a];
+        size[root_a] += arr[root_b];
+    }
+}
+
+int main() {}
