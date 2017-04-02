52453e7daa26ef4e817c59acf8a35e2555664d59
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-01 23:18:00 -0500

    Implement useful function to compare double in order to prevent problems with precision.

---
+++ Snippets/compare_doubles.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+using namespace std;
+const double EPS = 1e-15;
+
+/*
+ * Return 
+ * -1  if x < y
+ *  0  if x == y 
+ *  1  if x > y 
+ */
+int cmp (double x, double y){
+    return (x <= y + EPS) ? (x + EPS < y) ? -1 : 0 : 1;
+}
+
+int main(){
+    double d1 = 0.00000000000212;
+    double d2 = 0.00000000000213;
+    int res = cmp(d1,d2);
+    if (res == 0){
+        printf("Equal \n");
+    }else if(res == 1){
+        printf("Greater\n");
+    }else {
+        printf("Less \n");
+    }
+
+}
