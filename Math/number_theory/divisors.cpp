8138c61eaa04ff4358d3ecaca1cb1265174f3191
David Castelblanco Benavides <cbdavides@gmail.com>
2017-04-01 20:09:18 -0500

    I can bet anything that this two implementations to get de list of divisors of a number is correct.

--- Math/number_theory/divisors.cpp
+++ Math/number_theory/divisors.cpp
@@ -1,19 +1,20 @@
 #include <cstdio>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 typedef vector<int> vi;
 
 vi divisores(int n) {
     vi d;
-    int r = ceil(sqrt(n));
+    int r = sqrt(n);
 
-    for(int i = 1; i < r; i++) {
+    for(int i = 1; i <= r; i++) {
         if(n % i == 0) {
             d.push_back(i);
 
-            if(i != r)
+            if((i * i) != n)
                 d.push_back(n / i);
         }
     }
@@ -21,7 +22,8 @@ vi divisores(int n) {
 }
 
 int main() {
-    vi divi = divisores(52132);
+    vi divi = divisores(10);
+    sort(divi.begin(), divi.end());
     for(int i=0; i<divi.size(); i++)
         printf("%d ", divi[i]);
     printf("\n");
--- Math/number_theory/divisors.py
+++ Math/number_theory/divisors.py
@@ -2,13 +2,19 @@ import math
 
 def listDivisors(n):
     divisors = []
-    lim = math.ceil(math.sqrt(n))
+    lim = int(math.sqrt(n))
 
-    for i in range(1, lim):
+    for i in range(1, lim + 1):
         if n % i == 0:
             divisors.append(i)
 
-            if i != lim:
+            if (i * i) != n:
                 divisors.append(n // i)
 
     return divisors
+
+def main():
+    d = listDivisors(851)
+    print(len(d))
+    print(d)
+main()
