394b070d7df78317d928e844c0224071b4a38ff6
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-08 22:19:42 -0500

    Implement useful script using stringstream.

---
+++ Snippets/stringstream.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main(){
+    string line;
+    while (getline(cin, line)){
+        stringstream ss(line);
+        string word;
+        int count = 0;
+        while ( ss >> word) count ++;
+        cout << endl << "# Words: " << count << endl;
+    }
+}
+
