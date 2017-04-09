cf0741389162707c379f75b2f7c68be57e66d2e0
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-08 22:27:23 -0500

    Insert useful script for windows computers, standard input output.

---
+++ Snippets/freeopen.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main (){
+    freopen("data.in", "r", stdin);
+    freopen("data.out", "w", stdout);
+    
+    return 0;
+
+}
