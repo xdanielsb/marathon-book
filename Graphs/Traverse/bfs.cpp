ad9bffc0ecfcd3cb8ea0cbf88c75a89647c876a6
xdanielsb <dfsantosbu@unal.edu.co>
2017-04-12 22:31:11 -0500

    Add a better version of bfs.

--- Graphs/Traverse/bfs.cpp
+++ Graphs/Traverse/bfs.cpp
@@ -1,77 +1,50 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
+#include <bits/stdc++.h>
+#define NUM_NODES 8
 using namespace std;
 
-typedef struct node {
-    string val;
-    bool visited = false;
-    vector < node >  neighbors ;
+vector < int > g[NUM_NODES];
+int d[NUM_NODES];
+
+void show(){
+    for (int i = 0; i < NUM_NODES; i++){
+        cout << i << " " << d[i] << endl;
+    }
 }
-
-typedef vector < node >  list_nodes;
-
-void bfs(node start){
-
-    queue<node> s;
-    s.push(start);
-    start.visited = true;
-    while (s.empty() == false){
-        node top = s.front();
-        s.pop();
-        cout << "Node " << top.val << " visited" << endl;
-
-        list_nodes n = top.neighbors;
-        //Check for the termination condition
-        for (node a: n){
-            if (a.visited == false){
-                s.push(a);
-                a.visited = true;
+/*
+ * s -> origin
+ * n -> number of nodes
+ */
+void bfs(int o, int n){
+    for (int i = 0; i<=n;  i++) d[i] = -1;
+    queue < int > q;
+    q.push(o);
+    d[o]  = 0; 
+    while(q.size() > 0){
+        int  cur  = q.front(); q.pop();
+        for ( int i = 0; i < g[cur].size(); i++){
+            int next = g[cur][i];
+            if(d[next] == -1){
+                d[next] = d[cur] +1;
+                q.push(next);
             }
         }
-
     }
-
 }
 
 int main(){
 
-    /*          a
-     *        /    \
-     *       b      c
-     *       |    /   \
-     *       d   e     f
-     *
-     */
+    g[0].push_back(1);
+    g[0].push_back(2);
+    g[0].push_back(3);
+    g[1].push_back(4);
+    g[1].push_back(5);
+    g[2].push_back(6);
+    g[3].push_back(7);
+    
+    bfs(0, NUM_NODES);
 
-    node a,b,c,d,e,f;
-    a.val = "a";
-    b.val = "b";
-    c.val = "c";
-    d.val = "d";
-    e.val = "e";
-    f.val = "f";
-
-    list_nodes la(2), lc(2), lb(1);
+    show();
 
 
-    lc[0] = e;
-    lc[1] = f;
-    c.neighbors = lc;
-
-    lb[0] = d;
-    b.neighbors = lb;
-
-
-    la[0] = b;
-    la[1] = c;
-    a.neighbors = la;
-
-
-
-    cout<< " bfs -> a" <<endl<<endl;
-    bfs(a);
-
-    return 0;
+    return 0 ;
 }
