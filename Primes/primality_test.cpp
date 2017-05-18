70cdb060af47c46c27a621d675bf32e68a3955bc
xdanielsb <dfsantosbu@unal.edu.co>
2017-05-18 14:51:43 -0500

    Add algorithm  primaly test .

---
+++ Primes/primality_test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+typedef long long ll;
+
+bool is_prime(ll n){
+    if (n < 2) return false;
+    if (n < 4) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    if (n < 25) return true;
+    for(int i = 5; i*i <= n; i += 6){
+        if(n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+int main(){
+    cout << is_prime(23234) << endl;
+    cout << is_prime(2) << endl;
+    cout << is_prime(7454) << endl;
+    cout << is_prime(976) << endl;
+    cout << is_prime(1973) << endl;
+    return 0;
+
+}
