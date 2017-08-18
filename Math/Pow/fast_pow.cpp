247fd9e4c39e4eec7f4246567d952ddd1b1d4a60
Daniel Santos <dfsantosbu@unal.edu.co>
2017-08-18 08:48:55 -0500

    Add algorithm for modular pow.

---
+++ Math/Pow/fast_pow.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ll modular_pow(ll  base, int exponent, ll  modulus){
+	ll result = 1;
+	while (exponent > 0){
+		/* if y is odd, multiply base with result */
+		if (exponent & 1)
+			result = (result * base) % modulus;
+		/* exponent = exponent/2 */
+		exponent = exponent >> 1;
+		/* base = base * base */
+		base = (base * base) % modulus;
+	}
+	return result;
+}
+
+
+int main(){
+  
+
+}
