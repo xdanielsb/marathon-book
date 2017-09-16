f876410f82496d43d224c828b5e7622faf42e860
Daniel Santos <dfsantosbu@unal.edu.co>
2017-09-16 10:03:15 -0500

    Improve generation of latex code.

--- generator.sh
+++ generator.sh
@@ -1,4 +1,7 @@
 find . | grep  -E "\.(cpp|java|py)$" | cut -c3-100 > content.txt
+
+python generator.py && gnome-open templateNotebook.pdf
+
 rm templateNotebook.log
 rm templateNotebook.out
 rm templateNotebook.toc
