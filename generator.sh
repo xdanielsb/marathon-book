5d746c093ee95001b51b8b3b4dab346a270194ad
Daniel Santos <dfsantosbu@unal.edu.co>
2017-09-16 10:15:01 -0500

    Improve quality of code, notebook.

--- generator.sh
+++ generator.sh
@@ -1,6 +1,6 @@
 find . | grep  -E "\.(cpp|java|py)$" | cut -c3-100 > content.txt
 
-python generator.py && gnome-open templateNotebook.pdf &&  rm content.txt
+python generator.py &&  rm content.txt && mv templateNotebook.pdf book.pdf && mv book.pdf notebook/ && gnome-open notebook/book.pdf
 
 rm templateNotebook.log
 rm templateNotebook.out
