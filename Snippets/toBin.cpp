226f7c66952b602b82d8ab381d5cd97a61102c8c
Daniel Santos <dfsantosbu@unal.edu.co>
2017-09-18 21:57:19 -0500

    Add useful algorithm to bin in c++.

---
+++ Snippets/toBin.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void toBin(int x){
+  for (int i =31; i>=0; --i){
+      cout << ((x&(1LL<<i))!=0);
+  }
+}
+
+int main (){
+  toBin(10);
+  return 0;
+}
