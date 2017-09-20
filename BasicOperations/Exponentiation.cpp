1cc4b5d67095f4f5c69ef30c1d11cf4d752e903d
Daniel Santos <dfsantosbu@unal.edu.co>
2017-09-20 08:52:16 -0500

    Add methods for gaussian elimination and Exponetiation.

---
+++ BasicOperations/Exponentiation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll expo(ll a, ll b, ll c){
+  if (b == 0) return 1;
+  if (b % 2 == 0) {
+    ll temp = expo(a, b/2, c);
+    return (temp * temp) % c;
+  } else {
+    ll temp = expo(a, b-1, c);
+    return (temp * a) % c;
+  }
+}
+int main(){
+  cout << expo(2, 100, 1025);
+  return 0;
+}
