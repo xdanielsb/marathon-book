c52bb05a1ef3edb98fec24b4c8fdf411e0b7f3bd
David Castelblanco Benavides <cbdavides@gmail.com>
2017-09-22 10:55:08 -0500

    Suffix array implementation added plus a function to find the Longest Common Substring between two strings and LCP function.

---
+++ Strings/SuffixArray.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+
+typedef pair<int, int> ii;
+typedef long long ll;
+
+string s;
+const int N_MAX = 1000;
+int n, m, sa[N_MAX], rk[N_MAX], lcp[N_MAX];
+ll rk2[N_MAX];
+
+bool cmp(int i, int j) {
+    return rk2[i] < rk2[j];
+}
+
+void suffix_array() {
+    for(int i=0; i<n; i++) {
+        sa[i] = i; rk[i] = s[i]; rk2[i] = 0;
+    }
+
+    for(int l=1; l<n; l<<=1) {
+        for(int i=0; i<n; i++) {
+            rk2[i] = ((ll) rk[i] << 32) + (i + l < n ? rk[i + l] : -1);
+        }
+        sort(sa, sa + n, cmp);
+        for(int i=0; i<n; i++) {
+            if(i > 0 && rk2[sa[i]] == rk2[sa[i - 1]])
+                rk[sa[i]] = rk[sa[i - 1]];
+            else rk[sa[i]] = i;
+        }
+    }
+}
+
+void build_lcp() {
+    for(int i=0; i<n; i++) rk[sa[i]] = i;
+    for(int i=0, l=0; i<n; i++) {
+        if(rk[i] > 0) {
+            int j = sa[rk[i] - 1];
+            while(s[i + l] == s[j + l]) l++;
+            lcp[rk[i]] = l;
+            if(l > 0) l--;
+        }
+    }
+}
+
+ii LCS() {
+    int i, ind = 0, lcs = -1;
+    for (i = 1; i < n; i++) {
+        if (((sa[i] < n - m - 1) != (sa[i - 1] < n - m - 1)) && lcp[i] > lcs) {
+            lcs = lcp[i]; ind = i;
+        }
+    }
+    return ii(lcs, ind);
+}
+
+int main() {
+    string a = "panaderia", b = "asdsadpanaderiaasdasd";
+    s = a + "#" + b + "$";
+    n = s.size(); m = b.size();
+    suffix_array(); build_lcp();
+    ii result = LCS();
+    cout << result.F << ' ' << result.S << endl;
+    cout << s.substr(sa[result.S], result.F) << '\n';
+}
