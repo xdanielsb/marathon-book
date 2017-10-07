2d0c98820001ab65379070e862ff1b7067010074
Daniel Santos <dfsantosbu@unal.edu.co>
2017-10-07 06:49:59 -0500

    Add Naive find over strings.

---
+++ Strings/NaiveFind.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  string needle = "CD", haystack ="MANICD";
+  if(haystack.find(needle) != string::npos) cout << "Gotcha!!!";
+  else cout << "Not Gotcha";
+  cout << endl;
+  return 0;
+}
