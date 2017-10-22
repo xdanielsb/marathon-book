fc6a5f2d0c0d5a76d2fc6b214d56fbcbeba4244a
Daniel Santos <dfsantosbu@unal.edu.co>
2017-10-21 22:35:03 -0500

    Add some useful ecuations over a circle.

---
+++ Geometry/utilities.cpp
@@ -0,0 +1,10 @@
+//Some useful equations
+int main(){
+  //area portion of a circle
+  A = pi*r^2 * (theta/(2*pi))
+  //area chord of a circle
+  A = R * R * acos((R - h)/R) - (R - h) * sqrt(2 * R * h - h * h)
+    // h is the height of the chord
+  
+
+}
