1af18d572f003a4bf895e6b99a47652c4465c9d8
Daniel Santos <dfsantosbu@unal.edu.co>
2017-10-22 23:49:50 -0500

    Add rotation matrix , clockwise and counterclockwise.

--- Geometry/utilities.cpp
+++ Geometry/utilities.cpp
@@ -4,7 +4,9 @@ int main(){
   A = pi*r^2 * (theta/(2*pi))
   //area chord of a circle
   A = R * R * acos((R - h)/R) - (R - h) * sqrt(2 * R * h - h * h)
-    // h is the height of the chord
-  
-
+    // h is the height of the chord h = R - hypot(x,y)
+  // rotation matrix counnterclockwise
+  [x' y'] = [[cost -sint] [sint cost]][x y]
+  // rotation matrix clockwise
+  [x' y'] = [[cost sint] [-sint cost]][x y]
 }
