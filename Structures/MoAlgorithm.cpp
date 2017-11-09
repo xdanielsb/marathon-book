23f0cc62c95911e465ba2f44cedaa0fcd1ab90f0
Daniel Santos <dfsantosbu@unal.edu.co>
2017-11-09 16:23:09 -0500

    Make Mo's algorithm more modular.

--- Structures/MoAlgorithm.cpp
+++ Structures/MoAlgorithm.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 #define endl '\n'
 #define debug(x) cout << #x << " " <<x <<endl
-
+#define MAX (1000005)
 using namespace std;
 
 int block;
-
-/*
-	Complexity O(N*sqrt(N))
-*/
+int cnt[MAX];
 
 struct query {
 	int left;
@@ -22,19 +19,30 @@ struct query {
 	}
 };
 
-
-
 typedef vector < int > vi;
 typedef vector < query > vq;
 
+int numDiff =0;
+vi arr;
+
+
+void add(int pos){
+	numDiff += arr[pos];
+}
+
+void remove(int pos){
+	numDiff -= arr[pos];
+}
+
+
 int main(){
 	#ifdef LOCAL
-		freopen("in", "r", stdin);
+		freopen("in.c", "r", stdin);
 	#endif
 	int numQueries, numElements;
 	//read elements
 	cin >> numElements >> numQueries;
-	vi arr(numElements);
+	arr.resize(numElements);
 	for( int i=0; i<numElements; i++){
 		cin >> arr[i];
 	}
@@ -52,29 +60,30 @@ int main(){
 	sort(qs.begin(), qs.end());
 	
 	//iterate over the queries
-	int currR = 0, currL =0, sum=0;
+	int currR = 0, currL =0;
+	
 	for (query q : qs){
 		int l = q.left, r = q.right;
 		//remove elements previos range left
 		while(currL< l){
-			sum -= arr[currL];
+			remove(currL);
 			currL++;
 		}
 		//Add elements in the current range
 		while(currL>l){
-			sum += arr[currL-1];
+			add(currL-1);
 			currL--;
 		}
 		while(currR<=r){
-			sum += arr[currR];
+			add(currR);
 			currR++;
 		}
 		//Remove elemnts previos range right
 		while(currR > r+1){
-			sum -= arr[currR-1];
+			remove(currR-1);
 			currR--;
 		}
-		printf("Sum  from [%d, %d]= %d \n", q.left+1, q.right+1, sum);
+		printf("Sum  from [%d, %d]= %d \n", q.left+1, q.right+1, numDiff);
 	}
 	
 	return 0;
